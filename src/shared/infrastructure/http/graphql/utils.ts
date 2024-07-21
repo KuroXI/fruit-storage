18c64483b4738e1a61de3317a29253cff2b399ed
KuroXI <vgonzales5157@gmail.com>
2024-07-21 21:50:55 +0800

    [dev] remove unnecessary function and fix return type of limit and amount

--- src/shared/infrastructure/http/graphql/utils.ts
+++ src/shared/infrastructure/http/graphql/utils.ts
@@ -1,34 +1,11 @@
-import type { FlattenMaps, Types } from "mongoose";
-import type { IFruitStorage } from "../../database/mongoose/models/FruitStorage";
-import type { IFruit } from "../../database/mongoose/models/Fruit";
-import type { Storage } from "../../../../modules/storage/domain/storage";
 import type { Fruit } from "../../../../modules/fruit/domain/fruit";
-
-export const toReturnQuery = (
-	storage: FlattenMaps<IFruitStorage> & {
-		_id: Types.ObjectId;
-	},
-	fruit: FlattenMaps<IFruit> & {
-		_id: Types.ObjectId;
-	},
-) => {
-	return {
-		id: storage._id,
-		limit: storage.limit,
-		amount: storage.amount,
-		fruit: {
-			id: fruit._id,
-			name: fruit.name,
-			description: fruit.description,
-		},
-	};
-};
+import type { Storage } from "../../../../modules/storage/domain/storage";
 
 export const parseReturn = (storage: Storage, fruit: Fruit) => {
 	return {
 		id: storage.storageId.getStringValue(),
-		limit: storage.limit.getStringValue(),
-		amount: storage.amount.getStringValue(),
+		limit: storage.limit.value,
+		amount: storage.amount.value,
 		fruit: {
 			id: fruit.fruitId.getStringValue(),
 			name: fruit.name.value,
